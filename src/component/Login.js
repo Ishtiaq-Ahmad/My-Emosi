0bf3ddc0863a893744f0e5d62e132407a7d6bf43
Basil <basil@sucasa.uk>
2022-04-02 21:17:20 +0500

    Firebase Settings Done Login Added

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
@@ -10,12 +10,47 @@ import { useNavigate } from "react-router-dom";
 import "../style/login.scss";
 import LoingImgae from "../assets/images/myEmosiLoginImage.png";
 import MyEmosiLogo from "../assets/images/myEmosi_logo.png";
+import { initializeApp } from "firebase/app";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyByanGPc_ZnkVN8L2TlC03jLCyhGh_D_gY",
+  authDomain: "refinedmh.firebaseapp.com",
+  projectId: "refinedmh",
+  storageBucket: "refinedmh.appspot.com",
+  messagingSenderId: "110422148240",
+  appId: "1:110422148240:web:f4c6926fbc9fd0ddc88415",
+  measurementId: "G-C4TYK25M80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 const Login = () => {
   const navigate = useNavigate();
-  const loginHandler = () => {
-    navigate("/home");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  // const loginHandler = () => {
+  //   navigate("/home");
+  // };
+
+  const logInWithEmailAndPassword = async () => {
+    // alert(email)
+    if (email && password && email==='admin@myemosi.com') {
+      try {
+        await signInWithEmailAndPassword(auth, email, password)
+        navigate("/home");
+    
+      } catch (err) {
+        alert(err.message);
+      }
+    }
+    else{
+      alert('Please provide valid credentials')
+    }
   };
+
   return (
     <div className="background_image">
       <Grid container spacing={2}>
@@ -29,12 +64,13 @@ const Login = () => {
               />
             </div>
 
-            <FormControl sx={{ m: 3 }} fullWidth variant="standard">
+            <div>
               <TextField
                 className="user_name"
                 id="input-with-icon-textfield"
                 label="Email"
                 placeholder="Email Address"
+                onChange={(event) => setEmail(event.target.value)}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
@@ -51,6 +87,7 @@ const Login = () => {
                 label="Password"
                 type="password"
                 placeholder="Password"
+                onChange={(event) => setPassword(event.target.value)}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
@@ -62,14 +99,14 @@ const Login = () => {
                 fullWidth
               />
               <Button
-                type="submit"
+                type="button"
                 variant="contained"
-                onClick={loginHandler}
+                onClick={logInWithEmailAndPassword}
                 style={{ background: "#7293f0", color: "white" }}
               >
                 Login
               </Button>
-            </FormControl>
+            </div>
           </div>
         </Grid>
         <Grid item xs={0} md={6} className="display_left_column">
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyByanGPc_ZnkVN8L2TlC03jLCyhGh_D_gY",
+  authDomain: "refinedmh.firebaseapp.com",
+  projectId: "refinedmh",
+  storageBucket: "refinedmh.appspot.com",
+  messagingSenderId: "110422148240",
+  appId: "1:110422148240:web:f4c6926fbc9fd0ddc88415",
+  measurementId: "G-C4TYK25M80"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
